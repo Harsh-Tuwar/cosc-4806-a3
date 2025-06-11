1f4d4a1c2f8c51b8e76abe1142eb1fbbaade1ef5
Harsh Tuwar <38749882+Harsh-Tuwar@users.noreply.github.com>
2025-06-11 18:43:49 +0000

    introduced a new model, access log

---
+++ app/models/AccessLog.php
@@ -0,0 +1,13 @@
+<?php
+  class AccessLog {
+    public function __construct() {
+    }
+
+    public function logAccess($username, $success) {
+      $db = db_connect();
+      $query = 'INSERT INTO access_logs (username, success_attempt, timestamp) VALUES (:username, :success)';
+      $stmt = $db->prepare($query);
+      $stmt->bindParam(':username', $username);
+      $stmt->bindParam(':success', $success);
+    }
+  }  
