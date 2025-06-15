b85774ec01d0cd3397b951e21f0aa1bfe4569ec8
Harsh Tuwar <38749882+Harsh-Tuwar@users.noreply.github.com>
2025-06-15 19:21:06 +0000

    added style for home.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,9 +1,9 @@
 <?php require_once 'app/views/templates/header.php' ?>
-<div class="container">
+<main role="main" class="container mt-5 flex-grow-1">
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Hey</h1>
+                <h1>Hey <?php echo $_SESSION['username']; ?> 👋</h1>
                 <p class="lead"> <?= date("F jS, Y"); ?></p>
             </div>
         </div>
@@ -14,5 +14,6 @@
             <p> <a href="/logout">Click here to logout</a></p>
         </div>
     </div>
+</main>
 
     <?php require_once 'app/views/templates/footer.php' ?>
