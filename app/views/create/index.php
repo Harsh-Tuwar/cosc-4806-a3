1599bcb9adb0c851a96f9a829a295a8a5321a748
Harsh Tuwar <38749882+Harsh-Tuwar@users.noreply.github.com>
2025-06-15 19:08:25 +0000

    refactored create.php with new styling

--- app/views/create/index.php
+++ app/views/create/index.php
@@ -1,40 +1,43 @@
-<?php require_once 'app/views/templates/headerPublic.php'?>
+    <?php require_once 'app/views/templates/headerPublic.php' ?>
 
-<main role="main" class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>Lets have an account created for you!</h1>
+    <main role="main" class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+
+          <div class="text-center mb-4">
+            <h1 class="h3">Let's create your account!</h1>
+          </div>
+
+          <?php if (isset($_SESSION['signup_error'])): ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+              <?php echo $_SESSION['signup_error']; ?>
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-        </div>
-    </div>
+          <?php endif; ?>
 
-    <?php if (isset($_SESSION['signup_error'])): ?>
-        <div class="alert alert-danger">
-            <?php echo $_SESSION['signup_error']; ?>
-        </div>
-    <?php endif; ?>
+          <div class="card shadow-sm">
+            <div class="card-body">
+              <form action="/create/new_account" method="post">
+                <div class="mb-3">
+                  <label for="username" class="form-label">Username</label>
+                  <input required type="text" class="form-control" name="username" id="username">
+                </div>
 
-    <div class="row">
-        <div class="col-sm-auto">
-        <form action="/create/new_account" method="post" >
-        <fieldset>
-          <div class="form-group">
-            <label for="username">Username</label>
-            <input required type="text" class="form-control" name="username">
+                <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input required type="password" class="form-control" name="password" id="password">
+                </div>
+
+                <div class="d-grid gap-2">
+                  <button type="submit" class="btn btn-primary">Create Account</button>
+                  <a href="/login" class="btn btn-outline-secondary">Already have an account? Sign in</a>
+                </div>
+              </form>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input required type="password" class="form-control" name="password">
-          </div>
-          <br>
-        <div class="col-lg-12">
-            <button type="submit" class="btn btn-primary">Create</button>
-            <a href="/login" class="btn btn-info" role="button">Already have an account? Sign in.</a> 
+
         </div>
-        </fieldset>
-        </form> 
       </div>
-    </div>
+    </main>
 
-<?php require_once 'app/views/templates/footer.php' ?>
+    <?php require_once 'app/views/templates/footer.php' ?>
