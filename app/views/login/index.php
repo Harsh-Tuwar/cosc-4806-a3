7fc53808ce4cc648ffeca28a2a382b662bd868c8
Harsh Tuwar <38749882+Harsh-Tuwar@users.noreply.github.com>
2025-06-15 19:02:38 +0000

    rewrote login.html with bootstrap style

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,42 +1,43 @@
-<?php require_once 'app/views/templates/headerPublic.php'?>
+	<?php require_once 'app/views/templates/headerPublic.php' ?>
 
-<main role="main" class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>You are not logged in</h1>
-            </div>
-        </div>
-    </div>
+	<main role="main" class="container mt-5">
+		<div class="row justify-content-center">
+			<div class="col-md-6">
 
-		<?php if (isset($_SESSION['login_error'])): ?>
-				<div class="alert alert-danger">
-						<?php 
-							echo $_SESSION['login_error'];
-						?>
+				<div class="text-center mb-4">
+					<h1 class="h3">You are not logged in</h1>
 				</div>
-		<?php endif; ?>
 
-		<div class="row">
-		    <div class="col-sm-auto">
-				<form action="/login/verify" method="post" >
-				<fieldset>
-					<div class="form-group">
-						<label for="username">Username</label>
-						<input required type="text" class="form-control" name="username">
+				<?php if (isset($_SESSION['login_error'])): ?>
+					<div class="alert alert-danger alert-dismissible fade show" role="alert">
+						<?php echo $_SESSION['login_error']; ?>
+						<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 					</div>
-					<div class="form-group">
-						<label for="password">Password</label>
-						<input required type="password" class="form-control" name="password">
+				<?php endif; ?>
+
+				<div class="card shadow-sm">
+					<div class="card-body">
+						<form action="/login/verify" method="post">
+							<div class="mb-3">
+								<label for="username" class="form-label">Username</label>
+								<input required type="text" class="form-control" name="username" id="username">
+							</div>
+
+							<div class="mb-3">
+								<label for="password" class="form-label">Password</label>
+								<input required type="password" class="form-control" name="password" id="password">
+							</div>
+
+							<div class="d-grid gap-2">
+								<button type="submit" class="btn btn-primary">Login</button>
+								<a href="/create" class="btn btn-outline-secondary">Create an account</a>
+							</div>
+						</form>
 					</div>
-					<br>
-					<div class="col-lg-12">
-							<button type="submit" class="btn btn-primary">Login</button>
-							<a href="/create" class="btn btn-info" role="button">Create an account.</a>
-					</div>
-				</fieldset>
-				</form> 
+				</div>
+
 			</div>
 		</div>
-	
-<?php require_once 'app/views/templates/footer.php' ?>
+	</main>
+
+	<?php require_once 'app/views/templates/footer.php' ?>
