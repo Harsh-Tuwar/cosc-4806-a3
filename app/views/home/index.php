47fe9ad2ec8109143ab1e9675b88274f93dba2fb
Harsh Tuwar <38749882+Harsh-Tuwar@users.noreply.github.com>
2025-06-15 19:25:56 +0000

    some more clean up for header.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,19 +1,20 @@
 <?php require_once 'app/views/templates/header.php' ?>
-<main role="main" class="container mt-5 flex-grow-1">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>Hey <?php echo $_SESSION['username']; ?> 👋</h1>
-                <p class="lead"> <?= date("F jS, Y"); ?></p>
-            </div>
-        </div>
-    </div>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <p> <a href="/logout">Click here to logout</a></p>
+<main class="container mt-5 flex-grow-1">
+  <div class="row justify-content-center">
+    <div class="col-md-8 text-center">
+      <div class="card shadow-sm border-0">
+        <div class="card-body py-5">
+          <h1 class="display-5 mb-3">Hey, <?php echo htmlspecialchars($_SESSION['username']); ?> 👋</h1>
+          <p class="lead text-muted mb-4"><?= date("F jS, Y"); ?></p>
+
+          <a href="/logout" class="btn btn-outline-danger">
+            Logout
+          </a>
         </div>
+      </div>
     </div>
+  </div>
 </main>
 
-    <?php require_once 'app/views/templates/footer.php' ?>
+<?php require_once 'app/views/templates/footer.php' ?>
